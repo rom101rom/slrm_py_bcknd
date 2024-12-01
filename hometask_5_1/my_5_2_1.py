95404f7f85b0513d0f5717f41d38cb1aff935239
rom101rom <rom101rom@yandex.ru>
2024-12-01 13:55:44 +0300

    Delete hometask_5_1 directory
    Fault name for dir 5_2

--- hometask_5_1/my_5_2_1.py
+++
@@ -1,7 +0,0 @@
-def my_range(stop: float, start: float = 0.0, step: float = 1.0):
-    '''my_range - the function takes as input 3 variables: start, stop and step. Return list of steps(float)'''
-    res = []
-    while start < stop:
-        res.append(start)
-        start += step
-    return res
