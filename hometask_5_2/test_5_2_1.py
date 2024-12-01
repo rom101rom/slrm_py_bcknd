e0141d2f6f04f891cbd337c82b95de09a6a27fb8
rom101rom <rom101rom>
2024-12-01 14:01:34 +0300

    solve flake8 127 chars len limitation, del spaces around = in test_5_2_1

--- hometask_5_2/test_5_2_1.py
+++ hometask_5_2/test_5_2_1.py
@@ -1,9 +1,9 @@
 from my_5_2_1 import my_range
 
 def test_my_range():
-    assert my_range(start = 10, stop = 15, step = 2)
-    assert my_range(15, step = 3)
+    assert my_range(start=10, stop=15, step=2)
+    assert my_range(15, step=3)
 
 def test_not_my_range():
-    assert not my_range(start = 10, stop = 1, step = -1)
-    assert not my_range(start = 10, stop = 5)
+    assert not my_range(start=10, stop=1, step=-1)
+    assert not my_range(start=10, stop=5)
