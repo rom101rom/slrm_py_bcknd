df5a06d6e5376466a1516a460f8e35982ba39182
rom101rom <rom101rom>
2024-12-07 22:14:20 +0300

    mod 6_2_3ht to recursive find .txt files in nested dirs

--- hometask_6_2/6_2_3/ht_6_2_3.py
+++ hometask_6_2/6_2_3/ht_6_2_3.py
@@ -1,19 +1,18 @@
-import argparse
 import os
+import argparse
 from is_valid import is_valid
 
-'''Func validate brackets in file or dir, that pointed in commandline arg'''
-
+'''Func validate brackets in file or recursive dir, that pointed in commandline arg'''
 parser = argparse.ArgumentParser(description='Validate brackets validation. Enter filename or dir for validate as first arg')
 parser.add_argument("target_pointer", type=str, help="place (dir or file) for validation")
 args = parser.parse_args()
-#args =  Namespace(target_pointer='.\\slrm_py_bcknd\\hometask_6_2\\6_2_3\\dir_asserts\\assert_2.txt')
+
 if os.path.isdir(args.target_pointer):
-    for i in os.listdir(args.target_pointer):
-        if i.endswith('.txt'):
-            with open(f'{args.target_pointer}{i}') as f:
-                if not is_valid(f.read()): print(f'{i}: Error! Brackets invalid!')
-                else: print(f'{i}: Valid!')
+    for root, dirs, files in os.walk(args.target_pointer):
+        for file in files:
+            if file.endswith('.txt'):
+                with open(f'{root}/\\{file}') as f:
+                    if not is_valid(f.read()): print(f'{file} Error! Brackets invalid!')
 else:
     with open(args.target_pointer) as f:
-        if not is_valid(f.read()): print('Error! Brackets invalid!')
+        if not is_valid(f.read()): print(f'{f.name[2::]} Error! Brackets invalid!')
